d351ea61cc7215660c9a7606c32bae9311d4885d
tigp <hnztkfc@gmail.com>
2022-12-13 17:55:13 +0300

    made a app sketch

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+// import { Provider } from 'react-redux';
+import { useAsync } from 'react-async-hook';
+import { GiphyFetch } from '@giphy/js-fetch-api';
+import { Gif, Carousel } from '@giphy/react-components';
+import { IGif } from '@giphy/js-types';
 
-function App() {
+import './App.css';
+const giphyFetch = new GiphyFetch("RsyDbYcUXdcY4tRffnY7w1wMR5bRmkaY");
+
+const OneGif = (): JSX.Element | null => {
+  const [gif, setGif] = useState<IGif | null>(null);
+  const [gifStore, setGifStore] = useState<any>(null);
+
+  useAsync(async () => {
+    const { data } = await giphyFetch.gif("71e2uFlpO2RpC02qRX");
+    setGif(data);
+  }, []);
+
+  const test = async () => {
+    const fetchGifs = (offset: number) => giphyFetch.search('dogs', { offset, limit: 5 });
+    const { data } = await fetchGifs(5);
+    setGifStore(data);
+    console.log(gifStore);
+  };
+  test();
+  return (gif && <Gif gif={gif} width={400} />);
+};
+
+const CarouselDemo = (): JSX.Element | null => {
+  const fetchGifs = (offset: number) => giphyFetch.search("dogs", { offset, limit: 10 });
+  return <Carousel fetchGifs={fetchGifs} gifHeight={200} gutter={6} />;
+}
+
+const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <OneGif />
+      <CarouselDemo />
+    </>
   );
 }
 
